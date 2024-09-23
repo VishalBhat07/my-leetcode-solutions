44b20e9c3da5b314e2c3546b90c3aa5d422845d0
VishalBhat07 <vishalbhat21092005@gmail.com>
2024-09-23 22:38:37 +0530

    540. Single Element in a Sorted Array

---
+++ 540. Single Element in a Sorted Array.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    int singleNonDuplicate(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        if (n == 1)
+            return nums[0];
+
+        if (nums[0] != nums[1])
+            return nums[0];
+
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
+
+        int l = 1;
+        int r = n - 2;
+        int mid = (l + r) / 2;
+
+        while (l <= r)
+        {
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+                return nums[mid];
+
+            if ((mid % 2 == 1 && nums[mid - 1] == nums[mid]) ||
+                (mid % 2 == 0 && nums[mid] == nums[mid + 1]))
+                l = mid + 1;
+            else
+                r = mid - 1;
+
+            mid = (l + r) / 2;
+        }
+        return -1;
+    }
+};
