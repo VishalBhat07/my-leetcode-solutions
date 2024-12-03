5f9e36883f855841f80e261462449433c19b7087
VishalBhat07 <vishalbhat21092005@gmail.com>
2024-12-03 22:32:48 +0530

    2109. Adding Spaces to a String

---
+++ 2109. Adding Spaces to a String.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+
+        stringstream result;
+        int spaceIndex = 0;
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (spaceIndex < spaces.size() && i == spaces[spaceIndex]) {
+                result << ' ';
+                ++spaceIndex;
+            }
+            result << s[i];
+        }
+        return result.str();
+    }
+};
